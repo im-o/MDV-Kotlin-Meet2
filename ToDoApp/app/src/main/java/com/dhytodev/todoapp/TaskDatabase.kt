03bec6ec4fd98a38579bf19757393c671ebccb1c
Fordyta Abubakar <dhytodev@gmail.com>
2019-11-15 09:12:10 +0800

    create task database

---
+++ ToDoApp/app/src/main/java/com/dhytodev/todoapp/TaskDatabase.kt
@@ -0,0 +1,34 @@
+package com.dhytodev.todoapp
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Task::class], version = 1,
+    exportSchema = false
+)
+abstract class TaskDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        private var INSTANCE: TaskDatabase? = null
+        private val lock = Any()
+
+        fun getInstance(context: Context): TaskDatabase {
+            if (INSTANCE == null) {
+                synchronized(lock) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        TaskDatabase::class.java, "todoapp.db"
+                    ).build()
+                }
+                return INSTANCE as TaskDatabase
+            }
+
+            return INSTANCE as TaskDatabase
+        }
+    }
+}
